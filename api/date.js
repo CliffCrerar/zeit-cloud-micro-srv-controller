1fe46dc6393b6b7f312038adcb59d67626fb73e4
Cliff <cliff.crerar@gmail.com>
2019-06-22 18:12:55 +0200

    Branch-rebases

--- api/date.js
+++ api/date.js
@@ -1,16 +1,15 @@
-const express = require( 'express' );
-const helmet = require( 'helmet' );
-const moment = require( 'moment' );
-const log = require()
+const express = require('express');
+const helmet = require('helmet');
+const moment = require('moment');
 
 const app = express();
 
-app.use( helmet() );
+app.use(helmet());
 
-app.get( '/date', ( req, res ) => {
-  res.set( 'Content-Type', 'text/html' );
-  const currentTime = moment().format( 'MMMM Do YYYY, h:mm:ss a' );
-  res.status( 200 ).send( currentTime );
-} );
+app.get('*', (req, res) => {
+  res.set('Content-Type', 'text/html');
+  const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+  res.status(200).send(currentTime);
+});
 
 module.exports = app;
--- utils/log.js
+++
@@ -1,8 +0,0 @@
-const ut = require('util');
-
-//ERROR
-
-const log = (...params)=>ut.log(...params);
-
-
-export {log};
